119d4fc1c1926b69808fbcdbf26d168e7cc1689e
Jay Patel <96169579+Jayu-patel@users.noreply.github.com>
2024-01-05 11:07:09 +0530

    Update conn.js

--- database/conn.js
+++ database/conn.js
@@ -1,7 +1,6 @@
 const mongoose = require('mongoose')
-const link = 'mongodb://127.0.0.1:27017/mernProject'
-const link2 = 'mongodb+srv://JayuPatel:mernpassmongo@cluster1.hvuu6u9.mongodb.net/mernProject'
-mongoose.connect(link2)
+const link = preocess.env.LINK
+mongoose.connect(link)
 
 const userSchema = new mongoose.Schema({
     username: {
